32cd7d758393d6a0bf40312538a02327dbf67c88
AlexUvelir <152393638+AlexUvelir@users.noreply.github.com>
2024-05-12 10:39:15 +0000

    дополнили Confing.py переменным окружением dotenv

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
-TOKEN = '6464963614:AAEv39brpqmsfqTaCShxnzgRhawxQEmAHV0'
+#TOKEN = 6464963614:AAEv39brpqmsfqTaCShxnzgRhawxQEmAHV0
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN: str = os.getenv("TOKEN")
+
+
+
+
+
+
