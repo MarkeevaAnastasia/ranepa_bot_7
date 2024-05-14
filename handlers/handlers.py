cec5bcd005929e3e0fd06c69fc91934d43bc832c
MarkeevaAnastasia <152394801+MarkeevaAnastasia@users.noreply.github.com>
2024-05-14 01:21:10 +0000

    Залогировали handlers.py

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,16 +1,23 @@
+import logging 
 from aiogram import Router
 from aiogram import types
 from aiogram.filters.command import Command
+
+#настройка логирования
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 #@dp.message(Command('start'))
 async def process_start_command(message:types. Message):
     """Привествие"""
     await message.answer('Привет!\nПриятно познакомиться')
+  logging.info(f`user{message.from_user.id} starts bot`)
 
 #@dp.message()
 async def echo_command(message: types.Message):
     """Эхо-ответ"""
     await message.answer(message.text)
+  logging.info(f`user{message.from_user.id} echoes {message.text}`))
 
 def register_message_handler(router: Router):
   """Маршрутизация"""
