8c78daf90c97de6e2cde78588bcc8b5933ebc03f
vika30122005 <147839749+vika30122005@users.noreply.github.com>
2024-05-16 14:49:29 +0000

    спрятали TOKEN

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
-TOKEN = 6464963614:AAEv39brpqmsfqTaCShxnzgRhawxQEmAHV0
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN: str = os.getenv("TOKEN")
-
-
+TOKEN = os.environ['TOKEN']
 
 
 
